61b218c752efb45b50ff27dcbe38b03a89954f93
Sowji-3 <118727241+Sowji-3@users.noreply.github.com>
2022-11-29 22:00:10 +0530

    Update checkscript.sh

--- ChargeVision/checkscript.sh
+++ ChargeVision/checkscript.sh
@@ -1,7 +1,7 @@
 WEBPAGE="https://cv3.chargevisiondev.com/"
 STDOUTFILE=".tempCurlStdOut"
 > $STDOUTFILE
-HTTPCODE=$(curl  --max-time 5 --silent --write-out %{response_code} --output "$STDOUTFILE" "$WEBPAGE")
+HTTPCODE=$(curl --max-time 5 --silent --write-out %{response_code} --output "$STDOUTFILE" "$WEBPAGE")
 CONTENT=$(<$STDOUTFILE)
 echo "HTTP CODE: "$HTTPCODE
 echo "CONTENT LENGTH: "${#CONTENT}" chars"
