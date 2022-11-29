f936843fe8e8b009b21b91336a354998759f7c67
Sowji-3 <118727241+Sowji-3@users.noreply.github.com>
2022-11-29 21:20:05 +0530

    Create checkscript.sh

---
+++ ChargeVision/checkscript.sh
@@ -0,0 +1,7 @@
+WEBPAGE="https://www.google.com/"
+STDOUTFILE=".tempCurlStdOut"
+> $STDOUTFILE
+HTTPCODE=$(curl --max-time 5 --silent --write-out %{response_code} --output "$STDOUTFILE" "$WEBPAGE")
+CONTENT=$(<$STDOUTFILE)
+echo "HTTP CODE: "$HTTPCODE
+echo "CONTENT LENGTH: "${#CONTENT}" chars"
