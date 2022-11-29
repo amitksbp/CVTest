253eb2bfa26339aef11a7d86bc39dc55d9a7fb9a
Sowji-3 <118727241+Sowji-3@users.noreply.github.com>
2022-11-29 22:01:20 +0530

    Update bashscript.sh

--- ChargeVision/bashscript.sh
+++ ChargeVision/bashscript.sh
@@ -1,3 +1,3 @@
-website= "https://cv3chargevisiondev.com"
+website= "https://cv3chargevisiondev.com/"
 code=$(curl --head --location --max-time 5--silent --write-out '%{response_code}' $website --output /dev/null)
 echo $code
