f9f21315650a9af58e60d994e3a419ad26f1282d
Sowji-3 <118727241+Sowji-3@users.noreply.github.com>
2022-12-05 14:59:30 +0530

    Update checkscript.sh

--- ChargeVision/checkscript.sh
+++ ChargeVision/checkscript.sh
@@ -3,7 +3,7 @@ STDOUTFILE=".tempCurlStdOut"
 > $STDOUTFILE
 HTTPCODE=$(curl --max-time 5 --silent --write-out %{response_code} --output "$STDOUTFILE" "$WEBPAGE")
 CONTENT=$(<$STDOUTFILE)
-HTTPLOC=$(curl --location --output "$STDOUTFILE" "$WEBPAGE")
+HTTPLOC=$(curl --location --output "$STDOUTFILE" "$WEBPAGE") 
 echo "HTTP LOC: "${#HTTPLOC}
 echo "HTTP CODE: "$HTTPCODE
 echo "CONTENT LENGTH: "${#CONTENT}" chars"
