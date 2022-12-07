868e3b5348b88cb4ee28147eefcf12562692c034
Sowji-3 <118727241+Sowji-3@users.noreply.github.com>
2022-12-07 20:10:12 +0530

    Update checkscript.sh

--- ChargeVision/checkscript.sh
+++ ChargeVision/checkscript.sh
@@ -10,11 +10,11 @@ HTTPCODE1=$(curl --max-time 5 --silent --write-out %{response_code} --output "$S
 #echo "HTTP LOC: "${#HTTPLOC}
 
 
-echo "HTTP CODE1: "$HTTPCODE1
+echo "HTTP CODE for CV3 : "$HTTPCODE1
 if test $HTTPCODE1 -eq 200; then
-    echo "HTTP STATUS CODE $HTTPCODE -> Chargevision3 website is up" # stdout
+    echo "HTTP STATUS CODE for CV3 $HTTPCODE -> Chargevision3 website is up" # stdout
 else
-    >&2 echo "HTTP STATUS CODE for CV3 $HTTPCODE -> Has something gone wrong?" #stderr
+    >&2 echo "HTTP STATUS CODE  $HTTPCODE -> Has something gone wrong?" #stderr
 fi
 #echo "CONTENT LENGTH: "${#CONTENT}" chars"
 
@@ -23,7 +23,7 @@ WEBPAGE2="http://www.chargevision5.com/"
 STDOUTFILE=".tempCurlStdOut"
 > $STDOUTFILE
 HTTPCODE2=$(curl --max-time 5 --silent --write-out %{response_code} --output "$STDOUTFILE" "$WEBPAGE2")
-echo "HTTP CODE2: "$HTTPCODE2
+echo "HTTP CODE for CV5: "$HTTPCODE2
 if test $HTTPCODE2 -eq 200 || test $HTTPCODE2 -eq 301 ; then
     echo "HTTP STATUS CODE for CV5 $HTTPCODE -> Chargevision5 website is up" # stdout
 else
