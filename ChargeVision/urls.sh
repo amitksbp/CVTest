ca0bfdf10b5374df18417811bd1a5e7ca072cd50
Sowji-3 <118727241+Sowji-3@users.noreply.github.com>
2022-12-07 20:31:03 +0530

    Update urls.sh

--- ChargeVision/urls.sh
+++ ChargeVision/urls.sh
@@ -1,1 +1,16 @@
-curl http://www.chargevision3.com/
+webpage1= "http://chargevision3.com/"
+webpage2= "http://chargevision5.com/"
+
+for ((webpage1; webpage2))
+
+do
+     > $STDOUTFILE
+     HTTPCODE1=$(curl --max-time 5 --silent --write-out %{response_code} --output "$STDOUTFILE" "$WEBPAGE1")
+     HTTPCODE2=$(curl --max-time 5 --silent --write-out %{response_code} --output "$STDOUTFILE" "$WEBPAGE2")
+     
+     
+    echo "HTTPCODE1:" $HTTPCODE1
+    echo "HTTPCODE2:" $HTTPCODE2
+    
+done
+
