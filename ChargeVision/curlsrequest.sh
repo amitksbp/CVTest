e68a0b3dae9b46afb3cf78801f892527de7acf01
Sowji-3 <118727241+Sowji-3@users.noreply.github.com>
2022-12-07 15:44:39 +0530

    Update curlsrequest.sh

--- ChargeVision/curlsrequest.sh
+++ ChargeVision/curlsrequest.sh
@@ -1,1 +1,6 @@
-curl --max-time 5 --silent --write-out %{response_code} "http://www.chargevision3.com/" 
+HTTPCODE=$(curl --max-time 5 --silent --write-out %{response_code} "http://www.chargevision3.com/" )
+echo "HTTPCODE:" $HTTPCODE
+if $HTTPCODE= "200" then
+echo "success"
+else
+echo "something's wrong"
